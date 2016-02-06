8b23c5ce9a7d46d11795ccbb246778e251758b1a
Eugen Mihailescu <eugenmihailescux@gmail.com>
2016-02-06 23:16:14 +0100

    "Release of v0.2.2-10"

--- upload.php
+++ upload.php
@@ -1,4 +1,40 @@
 <?php
+/**
+ * ################################################################################
+ * WP MyBackup
+ * 
+ * Copyright 2016 Eugen Mihailescu <eugenmihailescux@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * ################################################################################
+ * 
+ * Short description:
+ * URL: http://wpmybackup.mynixworld.info
+ * 
+ * Git revision information:
+ * 
+ * @version : 0.2.2-10 $
+ * @commit  : dd80d40c9c5cb45f5eda75d6213c678f0618cdf8 $
+ * @author  : Eugen Mihailescu <eugenmihailescux@gmail.com> $
+ * @date    : Mon Dec 28 17:57:55 2015 +0100 $
+ * @file    : upload.php $
+ * 
+ * @id      : upload.php | Mon Dec 28 17:57:55 2015 +0100 | Eugen Mihailescu <eugenmihailescux@gmail.com> $
+*/
+
+namespace MyBackup;
+
 require_once MISC_PATH . 'MyChunkUploader.php';
 $uploader = new MyChunkUploader();
 $uploader->run();

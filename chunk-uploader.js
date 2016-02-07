f623a0492ead2a597b8c73ba8e64e3eca3ef7b53
Eugen Mihailescu <eugenmihailescux@gmail.com>
2016-02-07 08:43:43 +0100

    "Release of v0.2.2-10"

--- chunk-uploader.js
+++ chunk-uploader.js
@@ -47,6 +47,31 @@ Number.prototype.toHMS = function() {
 	return h.padTime() + ':' + m.padTime() + ':' + s.padTime();
 };
 
+// http://stackoverflow.com/questions/18638900/javascript-crc32
+String.prototype.crc32 = function() {
+	var makeCRCTable = function() {
+		var c;
+		var crcTable = [];
+		for (var n = 0; n < 256; n++) {
+			c = n;
+			for (var k = 0; k < 8; k++) {
+				c = ((c & 1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1));
+			}
+			crcTable[n] = c;
+		}
+		return crcTable;
+	};
+
+	var crcTable = window.crcTable || (window.crcTable = makeCRCTable());
+	var crc = 0 ^ (-1);
+
+	for (var i = 0; i < str.length; i++) {
+		crc = (crc >>> 8) ^ crcTable[(crc ^ str.charCodeAt(i)) & 0xFF];
+	}
+
+	return (crc ^ (-1)) >>> 0;
+};
+
 // http://en.wikipedia.org/wiki/XMLHttpRequest
 if (typeof XMLHttpRequest === 'undefined') {
 	XMLHttpRequest = function() {

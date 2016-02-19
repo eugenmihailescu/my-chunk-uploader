97c67a043392602b198a3f8473360015733d10d3
Eugen Mihailescu <eugenmihailescux@gmail.com>
2016-02-19 23:37:24 +0100

    "Release of v0.2.3-8"

--- MyChunkUploader.php
+++ MyChunkUploader.php
@@ -27,10 +27,10 @@
  * @version : 0.2.3-8 $
  * @commit  : 010da912cb002abdf2f3ab5168bf8438b97133ea $
  * @author  : Eugen Mihailescu eugenmihailescux@gmail.com $
- * @date    : Fri Feb 19 22:35:21 2016 UTC $
+ * @date    : Fri Feb 19 22:37:22 2016 UTC $
  * @file    : MyChunkUploader.php $
  * 
- * @id      : MyChunkUploader.php | Fri Feb 19 22:35:21 2016 UTC | Eugen Mihailescu eugenmihailescux@gmail.com $
+ * @id      : MyChunkUploader.php | Fri Feb 19 22:37:22 2016 UTC | Eugen Mihailescu eugenmihailescux@gmail.com $
 */
 
 namespace MyChunkUploader;

8fec53b220a3daa08baf4a2cdee92598ebbaef11
Eugen Mihailescu <eugenmihailescux@gmail.com>
2016-02-21 08:35:14 +0100

    "Release of v0.2.3-8"

--- chunk-uploader.js
+++ chunk-uploader.js
@@ -26,10 +26,10 @@
  * @version : 0.2.3-8 $
  * @commit  : 010da912cb002abdf2f3ab5168bf8438b97133ea $
  * @author  : Eugen Mihailescu eugenmihailescux@gmail.com $
- * @date    : Sun Feb 21 7:28:48 2016 UTC $
+ * @date    : Sun Feb 21 7:35:13 2016 UTC $
  * @file    : chunk-uploader.js $
  * 
- * @id      : chunk-uploader.js | Sun Feb 21 7:28:48 2016 UTC | Eugen Mihailescu eugenmihailescux@gmail.com $
+ * @id      : chunk-uploader.js | Sun Feb 21 7:35:13 2016 UTC | Eugen Mihailescu eugenmihailescux@gmail.com $
 */
 
 

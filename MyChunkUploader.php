45315edf5c10458c30407cfa5fc079ca554b7f12
eugenmihailescu <eugenmihailescux@gmail.com>
2016-03-03 15:56:02 +0100

    "Release of v0.2.3-16"

--- MyChunkUploader.php
+++ MyChunkUploader.php
@@ -45,6 +45,56 @@ if ( ! function_exists( __NAMESPACE__ . '\\_esc' ) ) {
 }
 
 /**
+ * Handling upload errors
+ *
+ * @class MyUploadException
+ * @extends Exception
+ *
+ * @since 1.0
+ * @version 1.0
+ * @package package_name
+ * @author Eugen Mihailescu
+ *        
+ */
+class MyUploadException extends Exception {
+
+	public function __construct( $error_code ) {
+		parent::__construct( $this->_getMessage( $error_code ), $error_code );
+	}
+
+	private function _getMessage( $error_code ) {
+		switch ( $error_code ) {
+			case UPLOAD_ERR_INI_SIZE :
+				$message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+				break;
+			case UPLOAD_ERR_FORM_SIZE :
+				$message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+				break;
+			case UPLOAD_ERR_PARTIAL :
+				$message = "The uploaded file was only partially uploaded";
+				break;
+			case UPLOAD_ERR_NO_FILE :
+				$message = "No file was uploaded";
+				break;
+			case UPLOAD_ERR_NO_TMP_DIR :
+				$message = "Missing a temporary folder";
+				break;
+			case UPLOAD_ERR_CANT_WRITE :
+				$message = "Failed to write file to disk";
+				break;
+			case UPLOAD_ERR_EXTENSION :
+				$message = "File upload stopped by extension";
+				break;
+			
+			default :
+				$message = "Unknown upload error";
+				break;
+		}
+		return $message;
+	}
+}
+
+/**
  * Process the inputs as slices of an chunked uploaded file
  *
  * @class MyChunkUploader
@@ -541,7 +591,7 @@ class MyChunkUploader {
 					return ( $a < $b ? - 1 : 1 ) * ( $desc ? - 1 : 1 );
 			} );
 		
-		return $parts;
+		return ! $parts ? array() : $parts;
 	}
 
 	/**
